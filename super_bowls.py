118ef46a39a043d5ef609afc0bbc7217d938387b
craighillelson <craighillelson@gmail.com>
2020-01-20 11:24:26 -0500

    updated super_bowls.py

--- super_bowls.py
+++ super_bowls.py
@@ -151,9 +151,11 @@ functions.score_math('final score margin', FINAL_SCORE_MARGIN)
 
 # upsets
 functions.header('upsets')
-for super_bowl, team_spread in sorted(UPSETS.items(), key=lambda x: x[1][1]):
-    team = team_spread[0]
-    print(f'Super Bowl {super_bowl}: {team} +{team_spread[1] * -1}')
+for super_bowl, team_line in sorted(UPSETS.items(), key=lambda x: x[1][1]):
+    team = team_line[0]
+    line = team_line[1] * -1
+    betting_line = '+' + str(line)
+    print(f'Super Bowl {super_bowl}: {team} {betting_line}')
 
 print(functions.RTN())
 
