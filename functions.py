b710adc17cdad25e5ab7777a8a86b2804fb09221
craighillelson <craighillelson@gmail.com>
2020-07-20 07:46:02 -0400

    update functions.py and super_bolws.py
    replace separate print return statements with inline returns

--- functions.py
+++ functions.py
@@ -4,8 +4,6 @@ import csv
 import operator
 from collections import namedtuple
 
-RTN = lambda: '\n'
-
 CURRENT_TEAMS_LST = []
 CURRENT_TEAMS_DCT = {}
 TEAMS = {}
@@ -39,7 +37,7 @@ FRANCHISES = set(CURRENT_TEAMS_DCT.keys()).intersection(TEAMS.keys())
 
 def header(title):
     """Print header."""
-    print(title.upper())
+    print(f'\n{title.upper()}')
 
 
 def yet_to_appear_or_win(list_headline, appeared_or_won, appeared_or_won_teams,
@@ -54,7 +52,6 @@ def yet_to_appear_or_win(list_headline, appeared_or_won, appeared_or_won_teams,
         nickname = city_nickname[-1]
         if nickname not in FRANCHISES:
             print(team)
-    print(RTN())
 
 
 def count(lst_, dct_):
@@ -69,7 +66,6 @@ def print_totals(category):
     for unit, total in sorted(sorted_totals, key=operator.itemgetter(1),
                               reverse=True):
         print(unit, total)
-    print(RTN())
 
 
 def score_math(total_or_diff_header, final_score):
@@ -78,7 +74,6 @@ def score_math(total_or_diff_header, final_score):
     for sb_number, total_or_diff in sorted(final_score.items(),
                                            key=lambda x: x[1], reverse=True):
         print(f"Super Bowl {sb_number}: {total_or_diff}")
-    print(RTN())
 
 
 def tally_and_print(category_header, team, team_tally):
