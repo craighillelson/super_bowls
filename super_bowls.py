b8db2011673e68933d8dfb32e69880d6f25d26b0
Craig Hillelson <craighillelson@gmail.com>
2021-12-14 09:52:09 -0500

    update super_bowls.py
    refactored

--- super_bowls.py
+++ super_bowls.py
@@ -1,162 +1,392 @@
-"""Query Super Bowl results."""
+"""
+Calculates statistics like most wins by a franchise, most wins by a coach, most
+mvps by player based on Super Bowl data.
+"""
 
-# imports
-from collections import namedtuple
 import csv
-import operator
-import functions
+from collections import Counter
+from collections import namedtuple
 
-# dictionaries
-SUPER_BOWLS = {}
-RESULTS = {}
-UPSETS = {}
-POINT_TOTAL = {}
-FINAL_SCORE_MARGIN = {}
-MVPS_TOTALS = {}
-TEAM_APPEARANCES = {}
-TEAM_LOSS_TOTALS = {}
-TEAM_WIN_TOTALS = {}
-COACH_LOSS_TOTALS = {}
-COACH_WIN_TOTALS = {}
-HOST_CITIES_TOTALS = {}
-SITES_TOTALS = {}
-MATCH_UPS_DCT = {}
+legacy_franchises = {
+    "Indianapolis Colts": "Baltimore Colts",
+    "Los Angeles Chargers": "San Diego Chargers",
+    "Las Vegas Raiders": ["Los Angeles Raiders", "Oakland Raiders"],
+    "Los Angeles Rams": "St. Louis Rams",
+    "Washington Football Team": "Washington Redskins",
+}
 
-# lists
-APPEARED = set()
-WON = set()
-YET_TO_APPEAR = []
-YET_TO_WIN = []
-WINNING_TEAMS = []
-LOSING_TEAMS = []
-MATCH_UPS = []
-MVPS = []
-COACHES_WON = []
-COACHES_LOST = []
-HOST_CITIES = []
-SITES = []
-OPPONENTS = []
 
-# import csv and populate a dictionary
-with open("csvs/super_bowls.csv", "r") as f:
-    F_CSV = csv.reader(f)
-    HEADINGS = next(F_CSV)
-    ROW = namedtuple('Row', HEADINGS)
-    for r in F_CSV:
-        row = ROW(*r)
-        RESULTS[row.superbowl] = [row.winner, row.winnerscore, row.loser,
-                                  row.loserscore]
-        match_up = row.winner, row.loser
-        MATCH_UPS.append(match_up)
-        WINNING_TEAMS.append(row.winner)
-        LOSING_TEAMS.append(row.loser)
-        APPEARED_TEAMS = WINNING_TEAMS + LOSING_TEAMS
-        MVPS.append(row.mvp)
-        COACHES_WON.append(row.winningcoach)
-        COACHES_LOST.append(row.losingcoach)
-        HOST_CITIES.append(row.city)
-        SITES.append(row.site)
-        point_differential = int(row.winnerscore) - int(row.loserscore)
-        FINAL_SCORE_MARGIN[row.superbowl] = point_differential
-        combined_score = int(row.winnerscore) + int(row.loserscore)
-        POINT_TOTAL[row.superbowl] = combined_score
-        team_appearances = (row.winner, row.loser)
-        if row.line != 'Pick \'em' and row.winner != row.favorite:
-            UPSETS[row.superbowl] = [row.winner, float(row.line)]
+def open_csv_and_populate_dct():
+    dct = {}
+
+    with open("super_bowls.csv") as csv_file:
+        f_csv = csv.reader(csv_file)
+        headings = next(f_csv)
+        assembled_tuple = namedtuple("assembled_tuple", headings)
+        for detail in f_csv:
+            row = assembled_tuple(*detail)
+            dct[row.date] = row.super_bowl, row.site, row.city, \
+                            row.visiting_team, row.visiting_team_score, \
+                            row.home_team, row.home_team_score, \
+                            row.mvp, row.winningcoach, row.losingcoach, \
+                            row.favorite, row.line
+
+    return dct
+
+
+def build_dct_of_franchises():
+    dct = {}
+
+    with open("franchises.csv") as csv_file:
+        f_csv = csv.reader(csv_file)
+        headings = next(f_csv)
+        assembled_tuple = namedtuple("assembled_tuple", headings)
+        for detail in f_csv:
+            row = assembled_tuple(*detail)
+            dct[row.team] = [row.alternate_names]
+
+    return dct
+
+
+def build_dictionary_of_teams_and_wins_or_losses(dct2):
+    dct1 = {}
+    for team, number_of_wins in dct2:
+        dct1.setdefault(team, number_of_wins)
+
+    return dct1
+
+
+def build_dictionary_of_upsets():
+    dct = {}
+    for super_bowl_name, details in winning_teams_favorites_and_lines.items():
+        winning_team = details[0]
+        winning_team_score = int(details[1])
+        losing_team = details[2]
+        losing_team_score = int(details[3])
+        favorite = details[4]
+        if details[5] == "Pick \'em":
+            line = details[5]
         else:
-            pass
+            line = float(details[5])
+            if winning_team != favorite and line != "Pick 'em":
+                dct[super_bowl_name] = [winning_team, winning_team_score,
+                                   losing_team, losing_team_score, favorite,
+                                   line]
 
-# results
-functions.header('results')
-for game, result in RESULTS.items():
-    winners = result[0]
-    winners_point_total = result[1]
-    losers = result[2]
-    losers_point_total = result[3]
-    print(f'Super Bowl {game}: {winners} {winners_point_total} - '
-          f'{losers} {losers_point_total}')
+    return dct
 
-# most appearances by team
-functions.tally_and_print('appearances', APPEARED_TEAMS, TEAM_APPEARANCES)
 
-# most common match ups
-functions.header('most common match ups')
-MATCH_UPS_LST = MATCH_UPS
-for teams in sorted(MATCH_UPS_LST):
-    opposing_teams = teams[0], teams[1]
-    opponents = sorted(opposing_teams)
-    OPPONENTS.append(opponents)
 
-for opposing_teams in OPPONENTS:
-    count = OPPONENTS.count(opposing_teams)
-    opponents = opposing_teams[0] + ' vs ' + opposing_teams[1]
-    MATCH_UPS_DCT[opponents] = count
+def build_list_of_host_cities():
+    lst = []
+    for details in super_bowls.values():
+        host_city = details[2]
+        lst.append(host_city)
 
-for opposing_teams, num_times_faced in sorted(MATCH_UPS_DCT.items(),
-                                              key=lambda x: x[1], reverse=True):
-    if num_times_faced > 1:
-        print(opposing_teams, num_times_faced)
-    else:
-        pass
+    return lst
 
-# calculate most consecutive appearances
 
-# teams yet to appear
-functions.yet_to_appear_or_win('yet to appear', APPEARED, APPEARED_TEAMS,
-                               YET_TO_APPEAR)
+def build_list_of_match_ups():
+    lst = []
+    for details in super_bowls.values():
+        teams = details[3], details[5]
+        lst.append(sorted(teams))
 
-# teams yet to win
-functions.yet_to_appear_or_win('yet to win', WON, WINNING_TEAMS, YET_TO_WIN)
+    return lst
 
-# winning teams ordered by number of wins
-functions.tally_and_print('team wins', WINNING_TEAMS, TEAM_WIN_TOTALS)
 
-# losing teams ordered by number of losses
-functions.tally_and_print('team losses', LOSING_TEAMS, TEAM_LOSS_TOTALS)
+def build_list_of_match_ups_sorted():
+    lst = []
+    for match_up in match_ups:
+        teams = f"{match_up[0]} vs {match_up[1]}"
+        lst.append(teams)
 
-# mvps
-functions.tally_and_print('mvps', MVPS, MVPS_TOTALS)
+    return lst
 
-# players to win multiple mvps
-functions.header('players to win multiple mvps')
-SORTED_MVP_TOTALS = sorted(MVPS_TOTALS.items(), key=operator.itemgetter(0))
-for player, number_of_mvp_trophies in sorted(SORTED_MVP_TOTALS,
-                                             key=operator.itemgetter(1),
-                                             reverse=True):
-    if number_of_mvp_trophies > 1:
-        print(player, number_of_mvp_trophies)
-    else:
-        pass
 
-# winning coaches ordered by number of wins
-functions.tally_and_print('winning coaches', COACHES_WON, COACH_WIN_TOTALS)
+def build_list_of_mvps():
+    lst = []
+    for details in super_bowls.values():
+        mvp = details[7]
+        lst.append(mvp)
 
-# losing coaches ordered by number of losses
-functions.tally_and_print('losing coaches', COACHES_LOST, COACH_LOSS_TOTALS)
+    return lst
 
-# coaches who've won and lost
-functions.header('coaches who\'ve won and lost')
-COACHES_WON_AND_LOST = set(COACHES_WON) & set(COACHES_LOST)
-for coach in sorted(COACHES_WON_AND_LOST):
-    print(coach)
 
-# total scores in descending order
-functions.score_math('total score', POINT_TOTAL)
+def build_lists_of_winning_and_losing_coaches():
+    lst1 = []
+    lst2 = []
+    for details in super_bowls.values():
+        winning_coach = details[8]
+        losing_coach = details[9]
+        lst1.append(winning_coach)
+        lst2.append(losing_coach)
 
-# final score margin in descending order
-functions.score_math('final score margin', FINAL_SCORE_MARGIN)
+    return lst1, lst2
 
-# upsets
-functions.header('upsets')
-for super_bowl, team_line in sorted(UPSETS.items(), key=lambda x: x[1][1]):
-    team = team_line[0]
-    line = team_line[1] * -1
-    BETTING_LINE = '+' + str(line)
-    print(f'Super Bowl {super_bowl}: {team} {BETTING_LINE}')
 
-# sites
-functions.tally_and_print('sites', SITES, SITES_TOTALS)
+def build_list_of_unique_values(lst):
+    return list(set(lst))
 
-# host cities
-functions.tally_and_print('host cities', HOST_CITIES, HOST_CITIES_TOTALS)
-print('\n')
+
+def build_lists_and_dictionary_of_winning_and_losing_teams():
+    lst1 = []
+    lst2 = []
+    dct = {}
+    print("\nsuper bowl results")
+    for details in super_bowls.values():
+        super_bowl_name = details[0]
+        visiting_team = details[3]
+        visiting_team_score = int(details[4])
+        home_team = details[5]
+        home_team_score = int(details[6])
+        mvp = details[7]
+        favorite = details[10]
+        line = details[11]
+        print(f"Super Bowl {super_bowl_name}")
+        print(f"{visiting_team} {visiting_team_score} vs {home_team} "
+              f"{home_team_score}")
+        if home_team_score > visiting_team_score:
+            print(f"winning team: {home_team}")
+            print(f"losing team: {visiting_team}")
+            print(f"mvp: {mvp}\n")
+            lst1.append(home_team)
+            lst2.append(visiting_team)
+            dct[super_bowl_name] = [home_team, home_team_score, visiting_team,
+                                    visiting_team_score, favorite, line]
+        else:
+            print(f"winning team: {visiting_team}")
+            print(f"losing team: {home_team}")
+            print(f"mvp: {mvp}\n")
+            lst1.append(visiting_team)
+            lst2.append(home_team)
+            dct[super_bowl_name] = [visiting_team, visiting_team_score,
+                                    home_team, home_team_score, favorite, line]
+
+    return lst1, lst2, dct
+
+
+def build_list_of_won_and_lost(lst2, lst3):
+    lst1 = []
+    for i in lst2:
+        if i in lst3:
+            lst1.append(i)
+
+    return lst1
+
+
+def calculate_combined_scores_and_score_differential_for_each_game():
+    dct1 = {}
+    dct2 = {}
+    for details in super_bowls.values():
+        super_bowl_name = details[0]
+        visiting_team_score = int(details[4])
+        home_team_score = int(details[6])
+        total_score = visiting_team_score + home_team_score
+        if visiting_team_score > home_team_score:
+            difference = visiting_team_score - home_team_score
+        else:
+            difference = home_team_score - visiting_team_score
+        dct1[super_bowl_name] = total_score
+        dct2[super_bowl_name] = difference
+
+    return dct1, dct2
+
+
+def count_wins_or_losses(lst2):
+    lst1 = Counter(lst2)
+
+    return lst1.most_common()
+
+
+def open_csv_and_populate_list():
+    lst = []
+
+    with open("current_teams.csv") as csv_file:
+        f_csv = csv.reader(csv_file)
+        headings = next(f_csv)
+        assembled_tuple = namedtuple("assembled_tuple", headings)
+        for detail in f_csv:
+            row = assembled_tuple(*detail)
+            lst.append(row.team)
+
+    return lst
+
+
+def output_dictionary_sorted_by_values_ascending(header, dct):
+    print(header)
+    for k, v in sorted(dct.items(), key=lambda x: x[1]):
+        print(k, v)
+
+
+def output_dictionary_sorted_by_values_descending(header, dct):
+    print(header)
+    for k, v in sorted(dct.items(), key=lambda x: x[1],
+                       reverse=True):
+        print(k, v)
+
+
+def output_franchises_that_have_appeared():
+    print("\nteams that have appeared")
+    for team in sorted(franchises_that_have_appeared):
+        print(team)
+
+
+def output_mvps_and_number_of_wins():
+    mvps = build_list_of_mvps()
+    mvp_counts = Counter(mvps)
+    mvps_counts = mvp_counts.most_common()
+
+    print("\nmvps")
+    for mvp in mvps_counts:
+        player = mvp[0]
+        number_of_wins = mvp[1]
+        print(f'{player}, {number_of_wins}')
+
+
+def output_list(header, lst):
+    print(header)
+    for i in sorted(lst):
+        print(i)
+
+
+def output_host_cities():
+    host_cities = build_list_of_host_cities()
+    city_counts = Counter(host_cities)
+    cities_counts = city_counts.most_common()
+
+    print("\nmost common host city")
+    for city_count in cities_counts:
+        city = city_count[0]
+        number_of_super_bowls_hosted = city_count[1]
+        print(f'{city}, {number_of_super_bowls_hosted}')
+
+
+def output_most_common_match_ups():
+    print("\nmost common match ups")
+    for match_ups_count in match_ups_counts:
+        if match_ups_count[1] > 1:
+            teams = match_ups_count[0]
+            number_of_super_bowls = match_ups_count[1]
+            print(f"{teams}, {number_of_super_bowls}")
+
+
+def output_teams_that_have_not_won_or_appeared(header, lst):
+    print(header)
+    for team in sorted(current_teams):
+        if team not in franchises_that_have_won and team not in \
+        list(legacy_franchises.keys()):
+            print(team)
+
+
+def output_upsets():
+    print("\nupsets")
+    for super_bowl_name, details in sorted(upsets.items(),
+                                           key=lambda x: x[1][5]):
+        winning_team = details[0]
+        winning_team_score = details[1]
+        losing_team = details[2]
+        losing_team_score = details[3]
+        line = details[5]
+        print(f"Super Bowl {super_bowl_name}"),
+        print(winning_team, winning_team_score, losing_team, losing_team_score)
+        print(f"line: {line}\n")
+
+
+def output_wins_or_losses_by_coach(header, lst):
+    print(header)
+    for coach_results in lst:
+        coach = coach_results[0]
+        result = coach_results[1]
+        print(coach, result)
+
+
+
+super_bowls = open_csv_and_populate_dct()
+
+winning_teams, losing_teams, winning_teams_favorites_and_lines = \
+build_lists_and_dictionary_of_winning_and_losing_teams()
+franchises_that_have_won = build_list_of_unique_values(winning_teams)
+franchises_that_have_lost = build_list_of_unique_values(losing_teams)
+
+output_list("franchises that have won", franchises_that_have_won)
+output_list("\nfranchises that have lost", franchises_that_have_lost)
+
+all_appearances = franchises_that_have_won + franchises_that_have_lost
+franchises_that_have_appeared = build_list_of_unique_values(all_appearances)
+output_list("\nfranchises that have appeared", franchises_that_have_appeared)
+
+franchises_that_have_won_and_lost = \
+build_list_of_won_and_lost(franchises_that_have_won, franchises_that_have_lost)
+
+output_list("\nfranchises that have both won and lost",
+            franchises_that_have_won_and_lost)
+
+winning_teams_number_of_wins = \
+count_wins_or_losses(winning_teams)
+
+team_number_of_wins = \
+build_dictionary_of_teams_and_wins_or_losses(winning_teams_number_of_wins)
+
+print("\nmost wins by a franchise")
+for team, number_of_wins in sorted(team_number_of_wins.items(),
+                                     key=lambda x: x[1], reverse=True):
+    print(team, number_of_wins)
+
+losing_teams_number_of_losses = \
+count_wins_or_losses(losing_teams)
+
+team_number_of_losses = \
+build_dictionary_of_teams_and_wins_or_losses(losing_teams_number_of_losses)
+
+print("\nmost losses by a franchise")
+for team, number_of_losses in sorted(team_number_of_losses.items(),
+                                     key=lambda x: x[1], reverse=True):
+    print(team, number_of_losses)
+
+current_teams = open_csv_and_populate_list()
+
+output_teams_that_have_not_won_or_appeared(
+"\ncurrent teams that have not won", franchises_that_have_won)
+
+output_teams_that_have_not_won_or_appeared(
+"\ncurrent teams that have not appeared", franchises_that_have_appeared)
+
+combined_scores, score_differentials = \
+calculate_combined_scores_and_score_differential_for_each_game()
+output_dictionary_sorted_by_values_descending("\ncombined scores descending",
+                                              combined_scores)
+output_dictionary_sorted_by_values_ascending("\ncombined scores ascending",
+                                              combined_scores)
+output_dictionary_sorted_by_values_descending("\nscore differential",
+                                              score_differentials)
+
+output_host_cities()
+output_mvps_and_number_of_wins()
+
+match_ups = build_list_of_match_ups()
+match_ups_sorted = build_list_of_match_ups_sorted()
+match_ups_count = Counter(match_ups_sorted)
+match_ups_counts = match_ups_count.most_common()
+output_most_common_match_ups()
+
+winning_coaches, losing_coaches = build_lists_of_winning_and_losing_coaches()
+
+coaches_who_have_won = build_list_of_unique_values(winning_coaches)
+output_list("\ncoaches who have won", coaches_who_have_won)
+
+coaches_who_have_lost = build_list_of_unique_values(losing_coaches)
+output_list("\ncoaches who have lost", coaches_who_have_lost)
+
+coaches_who_have_won_and_lost = \
+build_list_of_won_and_lost(coaches_who_have_won, coaches_who_have_lost)
+output_list("\ncoaches who have won and lost", coaches_who_have_won_and_lost)
+
+wins_by_coach = count_wins_or_losses(winning_coaches)
+output_wins_or_losses_by_coach("\nwins by coach", wins_by_coach)
+
+losses_by_coach = count_wins_or_losses(losing_coaches)
+output_wins_or_losses_by_coach("\nlosses by coach", losses_by_coach)
+
+upsets = build_dictionary_of_upsets()
+output_upsets()
